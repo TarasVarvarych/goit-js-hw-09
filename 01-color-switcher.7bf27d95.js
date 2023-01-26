e7193395cf7ef4c600ddb0d682d3ddf7cf74a8cb
TarasVarvarych <112963105+TarasVarvarych@users.noreply.github.com>
2023-01-26 20:34:12 +0000

    Deploying to gh-pages from @ TarasVarvarych/goit-js-hw-09@7877abc06a473a753ee587cf5329c3a08b1bfcda 🚀

---
+++ 01-color-switcher.7bf27d95.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;function r(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){n.style.backgroundColor=r(),o=setInterval((function(){n.style.backgroundColor=r()}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(o),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.7bf27d95.js.map
