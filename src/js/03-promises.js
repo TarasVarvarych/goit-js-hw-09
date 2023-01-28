7364d56f84b805d1693fa30b7e3324cd9130b110
TarasVarvarych <varvarychtaras@gmail.com>
2023-01-28 10:25:20 +0200

    123

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,47 +1,36 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formRef = document.querySelector('.form');
-let MAX_COUNTS = 0;
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve({ position, delay });
       } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject({ position, delay });
       }
     }, delay);
-  })
-    .then(x => Notify.success(x))
-    .catch(y => Notify.failure(y));
+  });
 }
 
 formRef.addEventListener('submit', onFormSubmit);
 function onFormSubmit(e) {
   e.preventDefault();
-  const {
-    elements: { delay, step, amount },
-  } = e.currentTarget;
-  const delayNumber = Number(delay.value);
-  const stepNumber = Number(step.value);
-  const amountNumber = Number(amount.value);
 
-  setTimeout(() => {
-    createPromise(1, delayNumber);
-    MAX_COUNTS += 1;
-    let intervalId = setInterval(() => {
-      if (MAX_COUNTS === amountNumber) {
-        clearInterval(intervalId);
-        return;
-      }
-      let position = 1 + MAX_COUNTS;
-      let del = delayNumber + stepNumber * MAX_COUNTS;
-      createPromise(position, del);
-      MAX_COUNTS += 1;
-    }, stepNumber);
-  }, delayNumber);
+  let delay = Number(formRef.delay.value);
+  let step = Number(formRef.step.value);
+  let amount = Number(formRef.amount.value);
 
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
   e.currentTarget.reset();
-  MAX_COUNTS = 0;
 }
